11a029cebed566ad03424d3ea3beb84c9de9cd22
tomascabreraleon <166922777+tomascabreraleon@users.noreply.github.com>
2024-04-21 13:31:48 +0200

    tomascabreraleon.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/tomascabreraleon.py
@@ -0,0 +1,14 @@
+# https://www.python.org/
+# Este es un comentario de una sola línea
+"""
+Este es un comentario
+de varias líneas.
+Se usa para comentarios extensos
+o docstrings (cadenas de documentación).
+"""
+'''
+Otra menra de hacer 
+comentarios 
+en Python.
+'''
+nombre= "Juan"
